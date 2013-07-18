18f8fb289ad0625f93ed1e3e62ea6c8b95a9b840
Ubuntu <ubuntu@ip-172-31-44-180.us-west-2.compute.internal>
2013-07-18 11:06:49 +0000

    finishing touches

--- grader.js
+++ grader.js
@@ -18,14 +18,16 @@ References:
  + JSON
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+h   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var REMOTEHTMLFILE_DEFAULT = "http://www.google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +57,47 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var remoteFile = function(htmlfile,checks){
+    
+    rest.get(htmlfile).on('complete', function(result) {
+	if (result instanceof Error) {
+	    sys.puts('Error: ' + result.message);
+	} else {
+//	    console.log(result);
+	    fs.writeFile("/tmp/myfile.html",result, function(err){
+		if (err) throw err;
+//		console.log('It\'s saved!');
+	    });
+	    //$ = cheerio.load(result);
+	    //var out = {};
+            //for(var ii in checks) {
+	//	var present = $(checks[ii]).length > 0;
+	//	out[checks[ii]] = present;
+          //  }
+        
+	    //var outJson = JSON.stringify(out, null, 4);
+	    //console.log(outJson);
+	}
+    });
+}
+
+var checkRemoteHtmlFile = function(htmlfile, checksfile) {
+   
+	remoteFile(htmlfile,checksfile);   
+
+    $ = cheerioHtmlFile('/tmp/myfile.html');
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+
+
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -64,11 +107,26 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <html_file>','Path to the URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var readFile1;
+    var checkJson;
+    if (!program.file && program.url != ''){
+	checkJson = checkRemoteHtmlFile(program.url,program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    
+}
+    else if (program.file != '' && !program.url){
+	checkJson = checkHtmlFile(program.file,program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+	
+    }else{
+	console.log("Bad usage");
+	return;
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
